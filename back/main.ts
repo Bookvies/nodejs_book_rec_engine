427eadb78649467b610f1b001e929e9795e5a54b
Andrew Lutsai <47223797+Alstrasz@users.noreply.github.com>
2021-11-29 12:57:46 +0300

    Experementing with port for heroku

--- back/main.ts
+++ back/main.ts
@@ -2,5 +2,5 @@ import * as server from './server';
 import { global_logger } from './logger';
 
 global_logger.info( 'Application started' );
-const s = new server.express_app( { ip: '127.0.0.1', port: 3030 } );
+const s = new server.express_app( { ip: '127.0.0.1', port: 8080 } );
 s.init().listen();
